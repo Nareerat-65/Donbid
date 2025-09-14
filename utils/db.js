4fddfc0a75d92634d9181e2dcef296c6212e109c
Nattawut <aumjahaha@gmail.com>
2025-09-14 23:18:36 +0700

    Update login styles for improved responsiveness and aesthetics; set database password in db.js

--- utils/db.js
+++ utils/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const db = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: '1234',
   database: 'donbid', // ใช้ตามจริง
 });
 
